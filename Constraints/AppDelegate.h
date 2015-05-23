0c72694ed731d849a37ab7418cd2dc77dceeeea2
Raven Smith <ravenesque2@gmail.com>
2015-05-23 19:53:33 -0400

    Initial Commit

---
+++ Constraints/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Constraints
+//
+//  Created by 乌雅 on 5/23/15.
+//  Copyright (c) 2015 Greenesttea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
